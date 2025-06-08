076b26f5cfad0ad7164ba98ae5cb935b733ac736
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2025-06-08 20:23:44 +0100

    venv and poetry install

--- ag-ui-travel-frontend/src/app/copilotkit/layout.tsx
+++ ag-ui-travel-frontend/src/app/copilotkit/layout.tsx
@@ -3,11 +3,12 @@ import React, { ReactNode } from "react";
 import { CopilotKit } from "@copilotkit/react-core";
 
 // Where CopilotKit will proxy requests to. If you're using Copilot Cloud, this environment variable will be empty.
-const runtimeUrl = process.env.NEXT_PUBLIC_COPILOTKIT_RUNTIME_URL;
+// const runtimeUrl = process.env.NEXT_PUBLIC_COPILOTKIT_RUNTIME_URL;
+const runtimeUrl = "/api/copilotkit";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <CopilotKit
+    <CopilotKit publicApiKey="ck_pub_01f0ba397100d16cd6e3310d0b62aab6"
       runtimeUrl={runtimeUrl}
       agent="travelAgent" // The name of the agent to use
     >
